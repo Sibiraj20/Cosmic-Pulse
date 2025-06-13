f1bbeb0f0b0f2f786cf92e3400a897a85e8d9ef9
Sibiraj <sibiraj19666@gmail.com>
2025-06-13 15:56:04 +0530

    Build changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Cosmic-Pulse/', // 👈 very important!
+  plugins: [react()]
 })
